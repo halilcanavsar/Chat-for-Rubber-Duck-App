27c8c16262a2b4ad586d433c0e9987cdb447388b
Halil Can Avşar <halilcanavsar@Halils-MacBook-Pro.local>
2022-06-27 12:20:06 +0300

    timestamp added

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,6 @@ io.on('connection', function (socket: any) {
   console.log('connected');
 
   socket.on('sendMessage', function (data: any) {
-    console.log(data);
     io.emit('receiveMessage', data);
   }
   );
